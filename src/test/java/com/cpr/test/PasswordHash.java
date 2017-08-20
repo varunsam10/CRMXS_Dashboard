46f89817aa1df391de9588427fb16d853486e9fc
sreekumar <sreekumar456@gmail.com>
2017-08-20 17:59:57 +0800

    Sree:Initial commit

---
+++ src/test/java/com/cpr/test/PasswordHash.java
@@ -0,0 +1,19 @@
+package com.cpr.test;
+
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+
+import junit.framework.TestCase;
+
+public class PasswordHash extends TestCase {
+	
+	
+	public void testMD5Hash(){
+		
+		String password="secret";
+		Md5PasswordEncoder passwordEnc=new Md5PasswordEncoder();
+		String hashedPasswd=passwordEnc.encodePassword(password,null);
+		System.out.println(hashedPasswd);
+		
+	}
+
+}
