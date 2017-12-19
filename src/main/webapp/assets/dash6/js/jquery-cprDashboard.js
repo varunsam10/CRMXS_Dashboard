6c6c43e3b2a8d711a5e507d14b9c025d1799cb54
sreekumar <sreekumar456@gmail.com>
2017-12-19 13:21:24 +0800

    Sree:Adding map pointers

--- src/main/webapp/assets/dash6/js/jquery-cprDashboard.js
+++ src/main/webapp/assets/dash6/js/jquery-cprDashboard.js
@@ -666,6 +666,60 @@
 							this._bindChartEventsC3(chartArea[0], widgetDefinition.widgetId, widgetDefinition, this);
 						}
 					}*/
+					map.addListener( "positionChanged", updateCustomMarkers );
+
+					// this function will take current images on the map and create HTML elements for them
+					function updateCustomMarkers( event ) {
+					  // get map object
+					  var map = event.chart;
+
+					  // go through all of the images
+					  for ( var x in map.dataProvider.images ) {
+					    // get MapImage object
+					    var image = map.dataProvider.images[ x ];
+
+					    // check if it has corresponding HTML element
+					    if ( 'undefined' == typeof image.externalElement )
+					      image.externalElement = createCustomMarker( image );
+
+					    // reposition the element accoridng to coordinates
+					    var xy = map.coordinatesToStageXY( image.longitude, image.latitude );
+					    image.externalElement.style.top = xy.y + 'px';
+					    image.externalElement.style.left = xy.x + 'px';
+					  }
+					}
+
+					// this function creates and returns a new marker element
+					function createCustomMarker( image ) {
+					  // create holder
+					  var holder = document.createElement( 'div' );
+					  holder.className = 'map-marker';
+					  holder.title = image.title;
+					  holder.style.position = 'absolute';
+
+					  // maybe add a link to it?
+					  if ( undefined != image.url ) {
+					    holder.onclick = function() {
+					      window.location.href = image.url;
+					    };
+					    holder.className += ' map-clickable';
+					  }
+
+					  // create dot
+					  var dot = document.createElement( 'div' );
+					  dot.className = 'dot';
+					  holder.appendChild( dot );
+
+					  // create pulse
+					  var pulse = document.createElement( 'div' );
+					  pulse.className = 'pulse';
+					  holder.appendChild( pulse );
+
+					  // append the marker to the map container
+					  image.chart.chartDiv.appendChild( holder );
+
+					  return holder;
+					}
 				}
 				else if(widgetDefinition.widgetType === 'static')
 				{
