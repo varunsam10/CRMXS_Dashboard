3ac3c6f34f25164ee234165d249930e47153b167
sreekumar <sreekumar456@gmail.com>
2018-01-06 12:03:30 +0800

    no message

--- src/main/java/com/cpr/controller/Dashboard6Controller.java
+++ src/main/java/com/cpr/controller/Dashboard6Controller.java
@@ -20,7 +20,7 @@ public class Dashboard6Controller {
 	@RequestMapping("/drawboard1")
 	public ModelAndView initializeForm() {
 		Dashboard3 dashboard3 = new Dashboard3();
-		System.out.println("inside dashboard 6!!");
+		//System.out.println("inside dashboard 6!!");
 		//DashboardDAO.getDashboardJson();
 		return new ModelAndView("dashboard6/drawboard1", "Dashboard3", new Dashboard3());
 	}
@@ -69,14 +69,14 @@ public class Dashboard6Controller {
 	@RequestMapping("/graphDetails")
 	public String graphDetailsPage() {
 
-		System.out.println("Inside the redirected link");
+		//System.out.println("Inside the redirected link");
 		return "dashboard6/graphDetails";
 	}
 
 	@RequestMapping("/widgetClick")
 	public String widgetClickDrawboard3() {
 		Dashboard3 widgetClick = new Dashboard3();
-		System.out.println("inside Widget click page ");
+		//System.out.println("inside Widget click page ");
 		return "forward:/graphDetails.html";
 	}
 
