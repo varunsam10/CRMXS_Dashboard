2b1551f2bf810ffea0a89a857f9d900934bf1eff
Manish-Ghumnani <maniac3493@gmail.com>
2018-02-07 11:32:41 +0800

    Added test class for DynamicDashboardCreationService

---
+++ src/test/java/com/cpr/service/test/DynamicDashboardCreationServiceTest.java
@@ -0,0 +1,33 @@
+package com.cpr.service.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cpr.service.DynamicDashboardCreationService;
+
+import junit.framework.Assert;
+
+public class DynamicDashboardCreationServiceTest {
+	
+	public DynamicDashboardCreationService dynamicDashboardService;
+	
+	@Before
+	public void setup(){
+		dynamicDashboardService =  new DynamicDashboardCreationService();
+	}
+	
+	@Test
+	public void testcreateDashboard2(){
+		assertEquals("Success", dynamicDashboardService.createDashboard2());
+	}
+	
+	@After
+	public void tearDown()
+	{
+		dynamicDashboardService = null;
+	}
+
+}
