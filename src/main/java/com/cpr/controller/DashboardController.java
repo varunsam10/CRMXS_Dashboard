6fad1bf7e6167b68e176f4a362df706ca05ff913
sreekumar <sreekumar456@gmail.com>
2018-02-07 11:04:59 +0800

    Sree:Adding additional service classes as per RMR and DMR

---
+++ src/main/java/com/cpr/controller/DashboardController.java
@@ -0,0 +1,67 @@
+package com.cpr.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.cpr.model.Dashboard3;
+import com.cpr.service.DashboardService;
+
+@Controller
+@SessionAttributes("/mainDashboard")
+public class DashboardController {
+
+	@Autowired
+	private DashboardService dashboardService;
+
+	@RequestMapping("/changeGraph")
+	public ResponseEntity changeGraphForWidgets(@RequestBody String widgetId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.changeGraphForId(widgetId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	@RequestMapping("/getWidgetBoards")
+	public ResponseEntity getWidgetBoards(@RequestBody String dashboardId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.getWidgetBoards(dashboardId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	@RequestMapping("/updateWidget")
+	public ResponseEntity updateWidgetDetails(@RequestBody String widgetId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.addWidgetById(widgetId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	@RequestMapping("/deleteWidget")
+	public ResponseEntity deleteWidgetFromDashboard(@RequestBody String widgetId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.deleteWidgetById(widgetId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	
+	@RequestMapping("/addWidget")
+	public ResponseEntity addWidgetToDashboard(@RequestBody String widgetId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.addWidgetById(widgetId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	@RequestMapping("/editWidget")
+	public ResponseEntity editWidgetTitleForWidget(@RequestBody String widgetId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.editWidgetTitleById(widgetId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	@RequestMapping("/downloadData")
+	public ResponseEntity downloadWidgetDataOfWidget(@RequestBody String widgetId) {
+		Dashboard3 dashboard3 = new Dashboard3();
+		String response = dashboardService.downloadWidgetDataofWidget(widgetId);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	
+	
+
+}
--- src/main/java/com/cpr/service/DashboardService.java
+++ src/main/java/com/cpr/service/DashboardService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cpr.model.DashboardDAO;
 import com.cpr.util.Dashboard;
 import com.cpr.util.Widget;
 import com.google.gson.Gson;
@@ -12,25 +14,61 @@ import com.google.gson.JsonElement;
 
 @Service
 public class DashboardService {
-	
+
 	@Autowired
 	private Dashboard dashboard;
 
-/*	public DashboardService() {
+	@Autowired
+	private DashboardDAO dashboardDAO;
 
-		dashboard = new Dashboard();
-	}*/
+	public String changeGraphForId(String widgetId) {
 
-	public Dashboard getDashboard() {
-		return dashboard;
+		String response = dashboardDAO.changeGraph(widgetId);
+		return response;
 	}
+
+	public String getWidgetBoards(String dashboardId) {
+
+		String response = dashboardDAO.getWidgetForDashboard(dashboardId);
+		return response;
+	}
+
+	public String updateWidgetById(String widgetId) {
+
+		String response = dashboardDAO.updateWidget(widgetId);
+		return response;
+	}
+
+	public String deleteWidgetById(String widgetId) {
+
+		String response = dashboardDAO.deleteWidget(widgetId);
+		return response;
+	}
+
+	public String addWidgetById(String widgetId) {
+
+		String response = dashboardDAO.addWidget(widgetId);
+		return response;
+	}
+
+	public String editWidgetTitleById(String widgetId) {
+
+		String response = dashboardDAO.editWidgetTitle(widgetId);
+		return response;
+	}
+	public String downloadWidgetDataofWidget(String widgetId) {
+		String response = dashboardDAO.downloadWidgetData(widgetId);
+		return response;
+	}
+	/*
+	 * public Dashboard getDashboard() { return dashboard; }
+	 */
 
 	public void setDashboard(Dashboard dashboard) {
 		this.dashboard = dashboard;
 	}
 
 	public String createDashboardJson() {
-
 		Gson gson = new GsonBuilder().create();
 		JsonElement element = gson.toJsonTree(dashboard.createDashboard());
 		String response = gson.toJson(element);
@@ -38,7 +76,6 @@ public class DashboardService {
 	}
 
 	public String createDashboardJson(ArrayList<Widget> widgets) {
-
 		Gson gson = new GsonBuilder().create();
 		JsonElement element = gson.toJsonTree(widgets);
 		String response = gson.toJson(element);
